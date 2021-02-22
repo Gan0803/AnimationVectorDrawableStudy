e057d8ca29ea8fe3e24194bdc6f003f8e2540f39
gan0803 <gan0803@gmail.com>
2021-02-22 10:18:19 +0900

    AnimatedVectorDrawableの状態を追加してトグルするように変更

--- app/src/main/java/gan0803/pj/study/animatedvectordrawablestudy/MainActivity.kt
+++ app/src/main/java/gan0803/pj/study/animatedvectordrawablestudy/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         const val STATE_RECT = 1
     }
 
-    private val state = STATE_TRIANGLE
+    private var state = STATE_TRIANGLE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +20,14 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<ImageButton>(R.id.imageButton).setOnClickListener {
             val imageButton = it as ImageButton
+            var drawable = R.drawable.triangle_to_rect
+            if (state == STATE_RECT) {
+                drawable = R.drawable.rect_to_triangle
+                state = STATE_TRIANGLE
+            } else {
+                state = STATE_RECT
+            }
+            imageButton.setImageDrawable(ResourcesCompat.getDrawable(resources, drawable, null))
             val animatable: Animatable = imageButton.drawable as Animatable
             animatable.start()
         }
