084c26262821d2ec9bf5e2f244404dfa19b82f6b
gan0803 <gan0803@gmail.com>
2021-02-22 10:11:34 +0900

    AnimatedVectorDrawableを追加

--- app/src/main/java/gan0803/pj/study/animatedvectordrawablestudy/MainActivity.kt
+++ app/src/main/java/gan0803/pj/study/animatedvectordrawablestudy/MainActivity.kt
@@ -1,11 +1,27 @@
 package gan0803.pj.study.animatedvectordrawablestudy
 
+import android.graphics.drawable.Animatable
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val STATE_TRIANGLE = 0
+        const val STATE_RECT = 1
+    }
+
+    private val state = STATE_TRIANGLE
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ImageButton>(R.id.imageButton).setOnClickListener {
+            val imageButton = it as ImageButton
+            val animatable: Animatable = imageButton.drawable as Animatable
+            animatable.start()
+        }
+
     }
 }
